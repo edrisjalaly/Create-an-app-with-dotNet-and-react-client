17f25ebb013074d3727eae42dfc50dba1fadef03
Edris <Edris@EDRIS-MIS>
2026-09-09 22:36:19 +0430

    add call api and iteration for received data

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
+import { useEffect, useState } from "react"
+
 function App() {
 
+  const [Activities, setActivites]= useState([]);
+
+  useEffect(()=> {
+    fetch("https://localhost:7201/api/Activities")
+    .then(response => response.json())
+    .then(data => setActivites(data));
+
+  }, []);
+
   return (
-    <>
-      An App
-    </>
-  )
+    <div>
+      <h3 className="app" style={{color: "red"}}> an-app</h3>
+      <ul>
+        { Activities.map((activity) => (
+          <li key={activity.id}> {activity.title} </li>
+        ))}
+      </ul>
+    </div>
+  ) 
 }
 
 export default App
