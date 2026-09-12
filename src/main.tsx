d568a273ab46cd4ddc1a749056fb1d22b907c025
Edris <Edris@EDRIS-MIS>
2026-09-12 20:02:48 +0430

    add ui packages and font for app

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import '@fontsource/roboto/300';
+import '@fontsource/roboto/400';
+import '@fontsource/roboto/500';
+import '@fontsource/roboto/700';
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
