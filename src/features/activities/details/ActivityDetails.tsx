73b8f85fcf1fa1ca7a1bd29fb695ef91c9bc84b3
Edris <Edris@EDRIS-MIS>
2026-09-20 22:58:05 +0430

    add activity details component

---
+++ src/features/activities/details/ActivityDetails.tsx
@@ -0,0 +1,26 @@
+import { Button, Card, CardActions, CardContent, CardMedia, Typography } from "@mui/material"
+
+type Props = {
+    activity: Activity
+}
+
+export default function ActivityDetails({activity}: Props) {
+  return (
+    <Card>
+     <CardMedia
+     component='img' 
+     src={`/images/categoryImages/${activity.category}.jpg`} />
+
+     <CardContent>
+      <Typography variant="h5">{activity.title}</Typography>
+      <Typography variant="subtitle1">{activity.date}</Typography>
+      <Typography variant="body1">{activity.description}</Typography>
+     </CardContent>
+     <CardActions>
+        <Button color="primary"> Edit </Button>
+        <Button color="primary"> Cancel </Button>
+     </CardActions>
+    </Card>
+ 
+  )
+}
